22afdf0929308f1afff486bd411db929a9f0d9cb
hanjo77 <hanjo77@gmail.com>
2015-04-30 22:19:21 +0200

    Updated database writer script

--- server/python/mqtt-mysql.py
+++ server/python/mqtt-mysql.py
@@ -6,6 +6,59 @@ import mysql.connector as mysql
 import datetime
 import json
 
+def get_connection():
+    return mysql.connect(user='root', host='127.0.0.1', database='easycab')
+
+def add_driver(token):
+    driver_id = 0
+    cnx = get_connection()
+    cursor = cnx.cursor()
+    try:
+        cursor.execute("INSERT INTO driver (token) VALUES ('" + token + "')")
+        driver_id = cursor.lastrowid
+    except:
+        driver_id = 0
+    cnx.commit()
+    cursor.close()
+    cnx.close()    
+    return driver_id
+
+def get_driver(token):
+    driver_id = 0
+    cnx = get_connection()
+    cursor = cnx.cursor()
+    result = cursor.execute("SELECT id FROM driver WHERE token = '" + token + "'")
+    for (id) in cursor:
+        driver_id = id[0]
+    cursor.close()
+    cnx.close()    
+    return driver_id
+
+def add_taxi(name):
+    taxi_id = 0
+    cnx = get_connection()
+    cursor = cnx.cursor()
+    try:
+        cursor.execute("INSERT INTO taxi (name) VALUES ('" + name + "')")
+        driver_id = cursor.lastrowid
+    except:
+        taxi_id = 0
+    cnx.commit()
+    cursor.close()
+    cnx.close()    
+    return taxi_id
+
+def get_taxi(name):
+    taxi_id = 0
+    cnx = get_connection()
+    cursor = cnx.cursor()
+    result = cursor.execute("SELECT id FROM taxi WHERE name = '" + name + "'")
+    for (id) in cursor:
+        taxi_id = id[0]
+    cursor.close()
+    cnx.close()    
+    return taxi_id
+
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
@@ -16,17 +69,26 @@ def on_connect(client, userdata, flags, rc):
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
-    print("Hallo")
-    cnx = mysql.connect(user='root', host='127.0.0.1', database='test')
+    cnx = get_connection()
     cursor = cnx.cursor()
     taxi = json.loads(msg.payload)
-    test = str(taxi['car'])
-    query = "INSERT INTO test (test) VALUES ('" + test + "')"
-    cursor.execute(query)
-    cnx.commit()
+    driver_id = get_driver(taxi['driver'])
+    if driver_id <= 0:
+        driver_id = add_driver(taxi['driver'])
+    taxi_id = get_taxi(taxi['car'])
+    if taxi_id <= 0:
+        taxi_id = add_taxi(taxi['car'])
+    query = "INSERT INTO position (taxi, driver, latitude, longitude) VALUES (%s, %s, %s, %s)"
+    parameters = (str(taxi_id), str(driver_id), str(taxi['gps']['latitude']), str(taxi['gps']['longitude']))
+    print(query)
+    try:
+        cursor.execute(query, parameters)
+        cnx.commit()
+        print("success")
+    except:
+        print("error")
     cursor.close()
-    cnx.close()
-    print(msg.topic+" "+str(msg.payload))
+    cnx.close()    
 
 client = mqtt.Client()
 client.on_connect = on_connect
