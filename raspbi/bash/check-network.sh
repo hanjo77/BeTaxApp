7c849a12ce3d7657b8fdb905dd61591ad239e718
hanjo77 <hanjo77@gmail.com>
2015-10-01 17:51:52 +0200

    Some fixes on Raspbi for automatic tethering.

--- raspbi/bash/check-network.sh
+++ raspbi/bash/check-network.sh
@@ -6,7 +6,17 @@ then
 	rm /var/run/gpsd.sock
 fi
 
+ERROR="$(ifconfig bnep0 | grep 'error')"
 NETWORK="$(ifconfig bnep0 | grep 'inet addr')"
+
+# There was an error, most possibly the network was not found because we have no thethered device so let's go back to tether
+
+if [ -z "${ERROR}" ]
+then
+    ./start-tethering.sh
+fi
+
+# Restart the bluetooth network
 
 if [ -z "${NETWORK}" ]
 then
--- raspbi/bash/start-tethering.sh
+++ raspbi/bash/start-tethering.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
-/etc/init.d/bluetooth restart
-sleep 10
-pand -c 00:61:71:D3:76:83 -role PANU --persist 30
+/etc/init.d/bluetooth restart;
+sleep 10;
+# pand -c 00:61:71:D3:76:83 -role PANU --persist 30
+pand --role PANU --search;
+echo "test";
